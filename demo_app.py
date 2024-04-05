c32d51ffe5b619734d0453395b933b593bd1c95c
Gabriel CHUA <gabriel_chua@tech.gov.sg>
2024-04-05 22:41:17 +0800

    minor UI change

--- demo_app.py
+++ demo_app.py
@@ -43,7 +43,7 @@ st.markdown("This demo uses a data.gov.sg dataset on HDB resale prices.", help="
 text_box = st.empty()
 qn_btn = st.empty()
 
-question = text_box.text_input("Ask a question", disabled=st.session_state.disabled)
+question = text_box.text_area("Ask a question", disabled=st.session_state.disabled)
 if qn_btn.button("Ask DAVE"):
 
     text_box.empty()
